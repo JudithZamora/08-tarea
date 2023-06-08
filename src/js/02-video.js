ca9eca19cbc2c90966754d6499f0d5646ecb16ad
Morzat Devourer <judith.zamora.go@gmail.com>
2023-06-07 19:45:25 -0600

    3th commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
+const saveCurrentTime = () => {
+  player.getCurrentTime().then((time) => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+};
+
+const loadCurrentTime = () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+};
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+window.addEventListener('load', loadCurrentTime);
 
